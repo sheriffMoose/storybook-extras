ceecf9140184352067270ade600a983f349d53f7
Mostafa Sherif <mosherif87@gmail.com>
2022-12-25 10:40:59 +0000

    initial commit: configure storybook v7 for angular

--- packages/angular/.storybook/main.js
+++ packages/angular/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  framework: "@storybook/angular",
+  stories: ["../app/**/*.stories.mdx", "../app/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: [{ from: "../.docs", to: "/docs" }],
   addons: ["../preset.js", "@storybook/addon-essentials"],
+  docs: { defaultName: "Overview", autodocs: true },
 };
