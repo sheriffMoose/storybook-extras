96170cfdbf838e90e9ce7548609e96681cecabab
Mostafa Sherif <mosherif87@gmail.com>
2022-12-26 22:27:04 -0500

    chore(infra): fix build

--- packages/angular/.storybook/main.js
+++ packages/angular/.storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   framework: "@storybook/angular",
+  core: { builder: '@storybook/builder-webpack5' },
   stories: ["../app/**/*.stories.mdx", "../app/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: [{ from: "../.docs", to: "/docs" }],
   addons: ["../preset.js", "@storybook/addon-essentials"],
