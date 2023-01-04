e84c5514be973bcc4008e2d2ab179d6867e07d6c
Mostafa Sherif <mosherif87@gmail.com>
2023-01-04 03:41:17 -0500

    docs(angular): update `README.md`

--- packages/angular/src/preset/manager.tsx
+++ packages/angular/src/preset/manager.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { addons, types } from "@storybook/addons";
-import { useAddonState, useChannel } from "@storybook/api";
-import { AddonPanel } from "@storybook/components";
-import { SourceCode, WrapperSelector } from "../components";
-import { ADDON_ID, EVENTS, PANEL_ID, TOOL_ID } from "../constants";
+import { addons, types } from '@storybook/addons';
+import { useAddonState, useChannel } from '@storybook/api';
+import { AddonPanel } from '@storybook/components';
+import { SourceCode, WrapperSelector } from '../components';
+import { ADDON_ID, EVENTS, PANEL_ID, SOURCE_CODE_PARAM_KEY, TOOL_ID, WRAPPERS_PARAM_KEY } from '../constants';
 
 const DEFAULT_STATE = { docs: null, components: [], services: [] };
 
 addons.register(ADDON_ID, (api) => {
   addons.add(TOOL_ID, {
-    title: "Wrappers",
+    title: 'Wrappers',
     type: types.TOOL,
+    paramKey: WRAPPERS_PARAM_KEY,
     match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
     render: WrapperSelector,
   });
 
   addons.add(PANEL_ID, {
     type: types.PANEL,
-    title: "Source Code",
-    match: ({ viewMode }) => viewMode === "story",
+    title: 'Source Code',
+    paramKey: SOURCE_CODE_PARAM_KEY,
+    match: ({ viewMode }) => viewMode === 'story',
     render: ({ active, key }) => {
       const [state, setState] = useAddonState(ADDON_ID, DEFAULT_STATE);
       useChannel({ [EVENTS.SET_CONTEXT]: context => setState(context) });
