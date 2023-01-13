5c49f966a96ccae2a9bc481413621ada4fd83358
Mostafa Sherif <mosherif87@gmail.com>
2023-01-13 20:24:29 +0000

    test: add extra wrappers for testing purposes

---
+++ packages/angular/app/border/border.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { BorderComponent } from './border.component';
+
+@NgModule({
+    exports: [BorderComponent],
+    declarations: [BorderComponent],
+})
+export class BorderModule { }
---
+++ packages/angular/app/highlight/highlight.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { HighlightComponent } from './highlight.component';
+
+@NgModule({
+    exports: [HighlightComponent],
+    declarations: [HighlightComponent],
+})
+export class HighlightModule { }
