9d38550a51e4bd0d30146983ce306e68f92a9513
Mostafa Sherif <mosherif87@gmail.com>
2023-01-22 09:40:12 +0000

    fix(angular): update `storybook-md` loader

--- packages/angular/preset.js
+++ packages/angular/preset.js
@@ -1,16 +1,1 @@
-const { addons, env, stories, webpackFinal } = require('./main');
-
-const preview = require.resolve("./dist/esm/preset/preview");
-const manager = require.resolve("./dist/esm/preset/manager");
-
-const managerEntries = (entry = []) => [...entry, manager];
-const previewAnnotations = (entry = []) => [...entry, preview];
-
-module.exports = {
-  stories,
-  addons,
-  env,
-  managerEntries,
-  previewAnnotations,
-  webpackFinal,
-};
+export * from './main';
