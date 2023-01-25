8476f9f47f66f4126ff1bc00bc1b226d0bc5eda9
Mostafa Sherif <mosherif87@gmail.com>
2023-01-24 22:07:11 -0500

    update angular addon

--- packages/angular/app/border/border.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { BorderComponent } from './border.component';
-
-@NgModule({
-    exports: [BorderComponent],
-    declarations: [BorderComponent],
-})
-export class BorderModule { }
--- packages/angular/main/index.js
+++ packages/angular/main/index.js
@@ -1,6 +1,3 @@
-export * from './addons';
-export * from './env';
 export * from './managerEntries';
 export * from './previewAnnotations';
-export * from './stories';
 export * from './webpackFinal';
