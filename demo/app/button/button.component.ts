2ebeb903245a32d796b2e603fae8ac9bf191a659
Mostafa Sherif <mosherif87@gmail.com>
2023-01-24 22:19:48 -0500

    add demo app

---
+++ demo/app/button/button.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ButtonService } from './button.service';
+
+@Component({
+    selector: 'storybook-button',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.scss'],
+    providers: [ButtonService],
+})
+export class ButtonComponent implements OnInit {
+    /**
+     * Is this the principal call to action on the page?
+     */
+    @Input()
+    primary = false;
+
+    /**
+     * What background color to use
+     */
+    @Input()
+    backgroundColor?: string;
+
+    /**
+     * How large should the button be?
+     */
+    @Input()
+    size: 'small' | 'medium' | 'large' = 'medium';
+
+    /**
+     * Button contents
+     *
+     * @required
+     */
+    @Input()
+    label = 'Button';
+
+    /**
+     * Optional click handler
+     */
+    @Output()
+    onClick = new EventEmitter<Event>();
+
+    constructor(private service: ButtonService) {}
+
+    public get classes(): string[] {
+        const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+        return ['storybook-button', `storybook-button--${this.size}`, mode];
+    }
+
+    ngOnInit() {
+        console.log('dev', 'this message will show only in the Actions panel');
+        console.log('test', 'this message will show only in the Developer Tools Console');
+    }
+}
