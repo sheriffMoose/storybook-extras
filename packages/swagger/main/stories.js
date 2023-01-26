7cdae2803c8351a31b40a7f05dd88e1e6c8bdd46
Mostafa Sherif <mosherif87@gmail.com>
2023-01-26 08:47:12 -0500

    chore(swagger): rename `openapi` story file

--- packages/swagger/main/stories.js
+++ packages/swagger/main/stories.js
@@ -1,6 +1,6 @@
 export const stories = (entries = [], options) => {
     if (options.openapiURL) {
-        entries.push(require.resolve('./openapi.mdx'));
+        entries.push(require.resolve('./openapi.story.mdx'));
     }
     return [...entries];
 };
