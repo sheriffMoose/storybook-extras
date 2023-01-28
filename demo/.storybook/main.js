06860be84c17bbbb4941528a1eac82028f5b2c5a
Mostafa Sherif <mosherif87@gmail.com>
2023-01-27 23:18:58 -0500

    chore: rename `app` to `src` in the demo app

--- demo/.storybook/main.js
+++ demo/.storybook/main.js
@@ -1,35 +1,35 @@
 module.exports = {
-    framework: '@storybook/angular',
-    stories: [
-        //
-        '../stories/**/*.@(md|html)',
-        '../app/**/*.stories.mdx',
-        '../app/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    staticDirs: [
-        {
-            from: '../.docs',
-            to: '/docs',
-        },
-    ],
-    addons: [
-        //
-        '@storybook-extras/preset',
-        '@storybook/addon-essentials',
-    ],
-    docs: {
-        defaultName: 'Overview',
-        autodocs: true,
-    },
-    extras: {
-        swagger: {
-            openapiURL: 'https://petstore.swagger.io/v2/swagger.json',
-        },
-        markdown: {
-            mdExclude: [/\.component\.html$/],
-            titles: {
-                '../stories/markdown-main-js.md': 'Markdown/main.js',
-            },
-        },
-    },
+  framework: '@storybook/angular',
+  stories: [
+      //
+      '../stories/**/*.@(md|html)',
+      '../src/**/*.stories.mdx',
+      '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  staticDirs: [
+      {
+          from: '../.docs',
+          to: '/docs',
+      },
+  ],
+  addons: [
+      //
+      '@storybook-extras/preset',
+      '@storybook/addon-essentials',
+  ],
+  docs: {
+      defaultName: 'Overview',
+      autodocs: true,
+  },
+  extras: {
+      swagger: {
+          openapiURL: 'https://petstore.swagger.io/v2/swagger.json',
+      },
+      markdown: {
+          mdExclude: [/\.component\.html$/],
+          titles: {
+              '../stories/markdown-main-js.md': 'Markdown/main.js',
+          },
+      },
+  },
 };
