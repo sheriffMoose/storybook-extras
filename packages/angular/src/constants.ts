52c648e365966968b78b746a04d30e758ac984ad
Mostafa Sherif <mosherif87@gmail.com>
2023-01-30 01:34:03 -0500

    feat(angular): update docs decorator to allow handling compodoc from main.js

--- packages/angular/src/constants.ts
+++ packages/angular/src/constants.ts
@@ -1,4 +1,4 @@
-export const ADDON_ID = "storybook/ng-extras";
+export const ADDON_ID = '@storybook-extras/angular';
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const PANEL_ID = `${ADDON_ID}/panel`;
 export const TAB_ID = `${ADDON_ID}/tab`;
@@ -7,9 +7,12 @@ export const DOCS_PARAM_KEY = `docs`;
 export const WRAPPERS_PARAM_KEY = `wrappers`;
 export const SOURCE_CODE_PARAM_KEY = `sourceCode`;
 
+export const COMPODOC_ID = '@storybook-extras/compodoc';
+export const COMPODOC_SOURCE_ID = `__COMPODOC_SOURCE__`;
+
 export const EVENTS = {
-  GET_DOCS: `${ADDON_ID}/getDocs`,
-  SET_DOCS: `${ADDON_ID}/setDocs`,
-  GET_CONTEXT: `${ADDON_ID}/getContext`,
-  SET_CONTEXT: `${ADDON_ID}/setContext`,
+    GET_DOCS: `${ADDON_ID}/getDocs`,
+    SET_DOCS: `${ADDON_ID}/setDocs`,
+    GET_CONTEXT: `${ADDON_ID}/getContext`,
+    SET_CONTEXT: `${ADDON_ID}/setContext`,
 };
--- packages/angular/src/decorators/docs.decorator.ts
+++ packages/angular/src/decorators/docs.decorator.ts
@@ -1,29 +1,51 @@
-import { setCompodocJson } from "@storybook/addon-docs/angular";
+import { setCompodocJson } from '@storybook/addon-docs/angular';
 import { addons, makeDecorator } from '@storybook/addons';
-import { DOCS_PARAM_KEY, EVENTS } from "../constants";
+import { COMPODOC_ID, COMPODOC_SOURCE_ID, DOCS_PARAM_KEY, EVENTS } from '../constants';
+import { DocsConfig } from '../types';
+import { deprecate } from '@storybook/client-logger';
+import { global } from '@storybook/global';
 
 export const docsDecorator = makeDecorator({
     name: 'docsDecorator',
     parameterName: '_docs',
     skipIfNoParametersOrOptions: false,
     wrapper: (storyFn, context) => {
-        const { parameters } = context;
-        const config = parameters[DOCS_PARAM_KEY];
+        const config: DocsConfig = context.parameters[DOCS_PARAM_KEY];
         const channel = addons.getChannel();
 
-        const setDocs = (docs) => {
+        const setDocs = docs => {
             setCompodocJson(docs);
             channel.emit(EVENTS.SET_DOCS, docs);
-        }
+        };
 
-        if (config?.data) {
+        const fetchDocs = url =>
+            fetch(config.fetch)
+                .then(docs => docs.json())
+                .then(docs => setDocs(docs));
+
+        // Precedence order: fetch > compodoc > data > lazyLoad > main.js
+        if (config?.fetch) {
+            global[COMPODOC_SOURCE_ID] = `parameters.docs.fetch`;
+            fetchDocs(config.fetch);
+        } else if (config?.compodoc) {
+            global[COMPODOC_SOURCE_ID] = `parameters.docs.compodoc`;
+            setDocs(config.compodoc);
+        } else if (config?.data) {
+            deprecate(`docsDecorator: 'data' has been renamed to 'compodoc'`);
+            global[COMPODOC_SOURCE_ID] = `parameters.docs.data`;
             setDocs(config.data);
         } else if (config?.url && config?.lazyLoad) {
-            fetch(config.url)
-                .then((docs) => docs.json())
-                .then((docs) => setDocs(docs));
+            deprecate(`docsDecorator: 'url' & 'lazyLoad' are  deprecated, use 'fetch' instead`);
+            global[COMPODOC_SOURCE_ID] = `parameters.docs.url`;
+            fetchDocs(config.url);
+        } else {
+            try {
+                global[COMPODOC_SOURCE_ID] = `main.docs.require`;
+                const docs = require(COMPODOC_ID);
+                setDocs(docs);
+            } catch (e) {}
         }
 
         return storyFn(context);
-    }
+    },
 });
