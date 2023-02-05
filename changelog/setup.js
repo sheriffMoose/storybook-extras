58fbca2a05cb236d199f8b27b56b90895340cbd1
Mostafa Sherif <mosherif87@gmail.com>
2023-02-04 19:06:36 -0500

    chore(infra): remove `handlebars-helpers`

--- changelog/setup.js
+++ changelog/setup.js
@@ -1,7 +1,5 @@
-require('handlebars-helpers')();
 
 module.exports = function (Handlebars) {
-    require('./helpers/href')(Handlebars);
     require('./helpers/scopes')(Handlebars);
     require('./helpers/unique')(Handlebars);
 };
