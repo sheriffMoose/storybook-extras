b878e4ff8789bb94d43a696ffb74358cc714f6ff
Mostafa Sherif <mosherif87@gmail.com>
2023-02-10 23:28:12 -0500

    refactor(devkit): use imports instead of require

--- packages/devkit/src/markdown/getMakeTitle.ts
+++ packages/devkit/src/markdown/getMakeTitle.ts
@@ -1,14 +1,15 @@
+import { resolve } from 'path';
+
 export const getMakeTitle = (fileName: string, addonOptions: any) => {
-    const { configDir, titles } = addonOptions;
+    const { configDir, stories } = addonOptions;
 
     return (userTitle: string) => {
-        const titleKey = Object.keys(titles || {}).find(key => require('path').resolve(configDir, key) === fileName) || '';
         const content = require('fs').readFileSync(fileName, 'utf8').toString();
 
         const htmlTitle = (content.match(/<title>(.*)<\/title>/i) || [])[1];
         const metaTitle = (content.match(/<meta title=[`'"“”‘’„”«»](.*)[`'"“”‘’„”«»] \/>/i) || [])[1];
         const markdownTitle = (content.match(/\{\/\*.*title:[`'"“”‘’„”«»](.*)[`'"“”‘’„”«»]\*\/\}/i) || [])[1];
-        const addonTitle = (titles || {})[titleKey];
+        const addonTitle = (stories || []).find(story => resolve(configDir, story.path) === fileName)?.title || '';
         const fileTitle = fileName.match(/\/([^\/]+)\.[^\/]+$/)[1].replace(/\+/g, '/');
 
         if (!userTitle || userTitle === fileName) {
