ba6406eff6fc51b1e32f4ecce209603e4d2cdf88
Mostafa Sherif <mosherif87@gmail.com>
2023-03-01 10:50:58 -0500

    feat(angular): add upgradeInsecureRequests

---
+++ packages/angular/src/previewHead.ts
@@ -0,0 +1,14 @@
+import { Options } from "@storybook/types";
+import { AngularConfig } from "./types";
+
+export const previewHead = (head, options: Options & AngularConfig) => {
+  const upgradeInsecureRequests =
+    options.upgradeInsecureRequests && options.configType === "PROD";
+
+  const metaTag = `<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />`;
+
+  return `
+    ${head}
+    ${upgradeInsecureRequests ? metaTag : ""}
+`;
+};
